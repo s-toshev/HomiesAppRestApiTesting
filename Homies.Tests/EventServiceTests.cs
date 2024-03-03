a5367e00151dec2114479f919c730802cf8a3f14
s-toshev <simeon.toshev@yahoo.bg>
2024-03-03 21:33:11 +0200

    3 Mar

--- Homies.Tests/EventServiceTests.cs
+++ Homies.Tests/EventServiceTests.cs
@@ -28,6 +28,7 @@ namespace Homies.Tests
         {
             // Step 1: Arrange - Set up the initial conditions for the test
             // Create a new event model with test data
+
             var eventModel = new EventFormModel
             {
                 Name = "Test Event",
@@ -35,7 +36,9 @@ namespace Homies.Tests
                 Start = DateTime.Now,
                 End = DateTime.Now.AddHours(2)
             };
+
             // Define a user ID for testing purposes
+
             string userId = "testUserId";
 
             // Step 2: Act - Perform the action being tested
@@ -45,7 +48,7 @@ namespace Homies.Tests
 
             // Step 3: Assert - Verify the outcome of the action
             // Retrieve the added event from the database
-            var eventInTheDataBase = _dbContext.Events.FirstOrDefaultAsync(x => x.Name == eventModel.Name && x.OrganiserId == userId);
+            var eventInTheDataBase = await _dbContext.Events.FirstOrDefaultAsync(x => x.Name == eventModel.Name && x.OrganiserId == userId);
 
 
             // Assert that the added event is not null, indicating it was successfully added
@@ -53,9 +56,9 @@ namespace Homies.Tests
             Assert.IsNotNull(eventInTheDataBase);
 
             // Assert that the description of the added event matches the description provided in the event model
-            Assert.Equals(eventModel.Description, eventModel.Description);
-            // Assert.That(eventInTheDataBase.Start, Is.EqualTo(eventModel.Start));
-            //  Assert.That(eventInTheDataBase.)
+            Assert.AreEqual(eventInTheDataBase.Description, eventModel.Description);
+            Assert.That(eventInTheDataBase.Start, Is.EqualTo(eventModel.Start));
+            Assert.That(eventInTheDataBase.End, Is.EqualTo(eventModel.End));
         }
 
 
@@ -66,10 +69,10 @@ namespace Homies.Tests
             // Create two event models with test data
             var firstEventModel = new EventFormModel
             {
-                Name = "Test Event",
-                Description = "Test Description",
+                Name = "First new Event",
+                Description = "new Description",
                 Start = DateTime.Now,
-                End = DateTime.Now.AddHours(2)
+                End = DateTime.Now.AddHours(7)
             };
 
             var secondEventModel = new EventFormModel
@@ -82,22 +85,21 @@ namespace Homies.Tests
 
 
             // Define a user ID for testing purposes
-            string userId = "testUserId";
+            string userId = "secondTestUserId";
 
-            await _eventService.AddEventAsync(firstEventModel, userId);
-            await _eventService.AddEventAsync(secondEventModel, userId);
+
             // Step 2: Act - Perform the action being tested
             // Add the two events to the database using the event service
 
-            var result = await _eventService.GetAllEventsAsync();
+            await _eventService.AddEventAsync(firstEventModel, userId);
+            await _eventService.AddEventAsync(secondEventModel, userId);
+
+            var result = await _dbContext.Events.CountAsync();
 
 
-            // Step 3: Act - Retrieve the count of events from the database
-
-            // Step 4: Assert - Verify the outcome of the action
             // Assert that the count of events in the database is equal to the expected count (2)
-
-            Assert.That(result.Count, Is.EqualTo(2));
+           
+            Assert.That(result, Is.EqualTo(2));
 
 
         }
@@ -116,7 +118,7 @@ namespace Homies.Tests
 
             };
 
-            await _eventService.AddEventAsync(firstEventModel, "nonExistingUserId");
+            await _eventService.AddEventAsync(firstEventModel, "1");
 
             var eventInTheDb = await _dbContext.Events.FirstAsync();
 
@@ -129,7 +131,7 @@ namespace Homies.Tests
             Assert.IsNotNull(result);
             Assert.That(result.Name, Is.EqualTo(firstEventModel.Name));
             Assert.That(result.Description, Is.EqualTo(firstEventModel.Description));
-
+            //I should add aditional prop checks
 
         }
 
@@ -137,21 +139,42 @@ namespace Homies.Tests
         public async Task GetEventForEditAsync_ShouldGetEventIfPresentInTheDb()
         {
             //Arrange
+            var firstEventModel = new EventFormModel
+            {
+                Name = "Test Event",
+                Description = "Test Description",
+                Start = DateTime.Now,
+                End = DateTime.Now.AddHours(2),
+                TypeId = 2,
+
+            };
+
+            await _eventService.AddEventAsync(firstEventModel, "1");
+
+            var eventInTheDb = await _dbContext.Events.FirstAsync();
 
             //Act
+            var result = await _eventService.GetEventForEditAsync(eventInTheDb.Id);
+
 
             //Assert
+            Assert.NotNull(result);
+
+            Assert.AreEqual(firstEventModel.Description, result.Description);
+            Assert.AreEqual(firstEventModel.Name, result.Name);
+            Assert.AreEqual(firstEventModel.Start, result.Start);
+            Assert.AreEqual(firstEventModel.End, result.End);
+
         }
 
 
         [Test]
         public async Task GetEventForEditAsync_ShouldReturnNullIfEventNotFound()
         {
-            //Arrange
-
             //Act
-
+            var result = await _eventService.GetEventForEditAsync(23);
             //Assert
+            Assert.Null(result);
         }
 
     }
